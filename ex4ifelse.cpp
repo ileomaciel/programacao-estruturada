3a2cd22685f9a0e438eb53f7e2dff5ad43ac0ad3
Leonardo <54076812+ileomaciel@users.noreply.github.com>
2025-05-18 22:28:10 -0300

    Update and rename main.cpp to ex4ifelse.cpp

--- ex4ifelse.cpp
+++ ex4ifelse.cpp
@@ -9,24 +9,24 @@ int main()
 
     int n1, n2, n3;
 
-    cout << "Digite o primeiro número: ";
+    cout << "Digite o primeiro nÃºmero: ";
     cin >> n1;
 
-    cout << "Digite o segundo número: ";
+    cout << "Digite o segundo nÃºmero: ";
     cin >> n2;
 
-    cout << "Digite o terceiro número: ";
+    cout << "Digite o terceiro nÃºmero: ";
     cin >> n3;
 
     if ((n1 >= n2) && (n1 >= n3)){
-        cout << "\nO maior número é: " << n1 << endl;
+        cout << "\nO maior nÃºmero Ã©: " << n1 << endl;
     }
     else{
         if ((n2 >= n1) && (n2 >= n3)){
-            cout << "\nO maior número é: " << n2 << endl;
+            cout << "\nO maior nÃºmero Ã©: " << n2 << endl;
             }
             else{
-                cout << "O maior número é: " << n3 << endl;
+                cout << "O maior nÃºmero Ã©: " << n3 << endl;
             }
     }
     return 0;
