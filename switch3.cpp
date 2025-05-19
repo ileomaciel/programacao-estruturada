128ee9c9a89cecbb3a4da6d259a06162522717c9
Leonardo <54076812+ileomaciel@users.noreply.github.com>
2025-05-18 22:41:31 -0300

    Update and rename main.cpp to switch3.cpp

--- switch3.cpp
+++ switch3.cpp
@@ -10,27 +10,27 @@ int main()
     char resultado;
     float n1, n2, soma, subtracao;
 
-    cout << "Escolha a operação que você deseja!!!" << endl;
+    cout << "Escolha a operaÃ§Ã£o que vocÃª deseja!!!" << endl;
     cout << "Digite + para SOMA" << endl;
-    cout << "Digite - para SUBTRAÇÃO" << endl;
+    cout << "Digite - para SUBTRAÃ‡ÃƒO" << endl;
     cin >> resultado;
 
-    cout << "Digite o primeiro número: ";
+    cout << "Digite o primeiro nÃºmero: ";
     cin >> n1;
-    cout << "Digite o segundo número: ";
+    cout << "Digite o segundo nÃºmero: ";
     cin >> n2;
 
     switch(resultado){
         case '+':
             soma = n1 + n2;
-            cout << "o resultado de " << n1 << " + " << n2 << " é: " << soma << endl;
+            cout << "o resultado de " << n1 << " + " << n2 << " Ã©: " << soma << endl;
         break;
         case '-':
             subtracao = n1 - n2;
-            cout << "o resultado de " << n1 << " - " << n2 << " é: " << subtracao << endl;
+            cout << "o resultado de " << n1 << " - " << n2 << " Ã©: " << subtracao << endl;
         break;
         default:
-            cout << "Opção Inválida!!! \n";
+            cout << "OpÃ§Ã£o InvÃ¡lida!!! \n";
     }
 
     return 0;
