1dc6cc31d601801384845b70352547da2ea6c9ed
Leonardo <54076812+ileomaciel@users.noreply.github.com>
2025-05-18 22:48:12 -0300

    Update and rename main.cpp to vetor6.cpp

--- vetor6.cpp
+++ vetor6.cpp
@@ -11,7 +11,7 @@ int main()
     int vetor[num];
 
     for(i = 0; i < num; i++){
-        cout << "Digite um número: ";
+        cout << "Digite um nÃºmero: ";
         cin >> vetor[i];
     }
 
@@ -19,7 +19,7 @@ int main()
         if(vetor[i] % 2 == 0){
             cout << vetor[i] << " - PAR" << endl;
         }else{
-            cout << vetor[i] << " - ÍMPAR" << endl;
+            cout << vetor[i] << " - ÃMPAR" << endl;
             }
         }
 
