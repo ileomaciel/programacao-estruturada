26a8cbd912dc37b6d5fca9c3b679f7dafea00409
Leonardo <54076812+ileomaciel@users.noreply.github.com>
2025-05-18 22:48:29 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <locale>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
+
+    int matriz[2][3];
+    int i, j;
+
+    for(i = 0; i < 2; i++){ //linha
+        for(j = 0; j < 3; j++){ //coluna
+            cout << "Digite um valor para preencher a matriz [" << i << ", " << j << "]: " << endl ;
+            cin >> matriz[i][j];
+        }
+    }
+
+    return 0;
+}
