20264e6c20b2abc68baf0a5143e2f3bc784da4db
Leonardo <54076812+ileomaciel@users.noreply.github.com>
2025-05-18 23:00:40 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <locale>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
+
+    int i, j, m;
+
+    cout << "Digite o número da MATRIZ que você deseja: ";
+    cin >> m;
+    int matriz[m][m];
+
+
+    for(i = 0; i < m; i++){ //linha
+        for(j = 0; j < m; j++){ //coluna
+            cout << "Digite um valor para preencher a matriz [" << i << ", " << j << "]: ";
+            cin >> matriz[i][j];
+        }
+    }
+    for(i = 0; i < m; i++){ //linha
+        for(j = 0; j < m; j++){ //coluna
+            if(matriz[i] == matriz[j]){
+                cout << matriz[i][j] << "\t";
+            }
+
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
