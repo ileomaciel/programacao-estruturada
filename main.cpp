982b0ba442be72dc148340a3af4e2a7e621ef6da
Leonardo <54076812+ileomaciel@users.noreply.github.com>
2025-05-18 22:19:24 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <locale>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
+
+    int golsSaoPaulo, golsPalmeiras;
+
+    cout << "***Placar São Paulo x Palmeiras ***" << endl;
+
+    cout << "Digite a quantidade de gols do São Paulo: ";
+    cin >> golsSaoPaulo;
+
+    cout << "Digite a quantidade de gols do Palmeiras: ";
+    cin >> golsPalmeiras;
+
+    if (golsSaoPaulo > golsPalmeiras){
+        cout << "\nA festa é Tricolor" << endl;
+    }
+    else{
+        cout << "\nA festa é do Palmeiras" << endl;
+
+    }
+    return 0;
+}
