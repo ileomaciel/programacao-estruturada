3ec50688f23c68d70f927f3170dc7a7a47233cad
Leonardo <54076812+ileomaciel@users.noreply.github.com>
2025-05-21 07:49:26 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <locale>
+
+using namespace std;
+
+void oi(void){
+    cout << "Oi, esse é o primeiro exemplo de def. de FUNCÇÃO em C++." << endl;
+    cout << "Ainda veremos outros exemplos." << endl << endl;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
+    int i = 1;
+
+    while(i <= 5){
+        oi();
+        i = i+1;
+
+        }
+
+    return 0;
+}
