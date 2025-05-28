9055a63373a9337d6d91dc285caf3e367a4443ce
Leonardo <54076812+ileomaciel@users.noreply.github.com>
2025-05-28 10:04:07 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+#include <string.h>
+#include <locale>
+
+using namespace std;
+
+int main(){
+
+    setlocale(LC_ALL, "Portuguese");
+
+    string abc;
+
+    ifstream arq_leitura("notas.txt");
+
+    if(arq_leitura.is_open()){
+        while(! arq_leitura.eof()){
+            getline(arq_leitura, abc);
+            cout << abc << endl;
+        }
+        arq_leitura.close();
+    }else{
+        cout << "ERRO: arquivo não foi aberto ou não existe" << endl;
+
+    }
+    return 0;
+}
