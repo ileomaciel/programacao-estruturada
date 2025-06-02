59daef4c38652f1a933bef72cef7ceb6c869f378
Leonardo <54076812+ileomaciel@users.noreply.github.com>
2025-06-01 21:16:49 -0300

    Create revisao11.cpp

---
+++ revisao11.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+float imc(float peso, float altura){
+    return (peso / (altura * altura));
+}
+
+int main(){
+
+    setlocale(LC_ALL, "Portuguese");
+
+    float peso, altura;
+        cout << "Programa para calcular o IMC de uma pessoa " << endl << endl;
+        cout << "Digite o peso e Kilos: ";
+        cin >> peso;
+        cout << "Digite a altura em Metros: ";
+        cin >> altura;
+        
+    cout << "\nO IMC é: " << imc(peso, altura) << endl;
+    
+    return 0;
+}
