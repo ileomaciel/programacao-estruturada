698b6ead5a9b8136f01b534fb55a26a094cb8fc0
Leonardo <54076812+ileomaciel@users.noreply.github.com>
2025-06-01 21:03:42 -0300

    Create revisao10.cpp

---
+++ revisao10.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <cstring>
+#include <locale>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+
+    setlocale(LC_ALL, "Portuguese");
+
+    int NANIMAL;
+
+    cout << "Quantos animais você deseja cadastrar?: ";
+    cin >> NANIMAL;
+    cin.ignore();
+
+    struct EstruturaAnimal {
+        char nome[50];
+        char especie[50];
+        char raca[50];
+        char sexo;
+        int idade;
+        char nomedono[50];
+        char rg[20];
+        char telefone[20];
+    };
+
+    EstruturaAnimal strAnimal[NANIMAL];
+
+    for (int i = 0; i < NANIMAL; i++) {
+        cout << "\nCadastro do PET #" << i + 1 << endl;
+        cout << "Informe o Nome do PET: ";
+        cin.getline(strAnimal[i].nome, 50);
+        cout << "Informe a Espécie do PET: ";
+        cin.getline(strAnimal[i].especie, 50);
+        cout << "Informe a Raça do PET: ";
+        cin.getline(strAnimal[i].raca, 50);
+        cout << "Informe o Sexo (M/F): ";
+        cin >> strAnimal[i].sexo;
+        cin.ignore();
+        cout << "Informe a Idade do PET: ";
+        cin >> strAnimal[i].idade;
+        cin.ignore();
+        cout << "Informe o Nome do DONO: ";
+        cin.getline(strAnimal[i].nomedono, 50);
+        cout << "Informe o RG do DONO: ";
+        cin.getline(strAnimal[i].rg, 20);
+        cout << "Informe o Telefone: ";
+        cin.getline(strAnimal[i].telefone, 20);
+
+        cout << endl;
+    }
+
+    ofstream arquivo;
+    arquivo.open("arquivo.txt");
+
+    cout << "\nANIMAIS Cadastrados com Sucesso!!!\n" << endl;
+
+    for (int i = 0; i < NANIMAL; i++) {
+        arquivo << "PET #" << (i + 1) << endl;
+        arquivo << "Nome: " << strAnimal[i].nome << endl;
+        arquivo << "Espécie: " << strAnimal[i].especie << endl;
+        arquivo << "Raça: " << strAnimal[i].raca << endl;
+        arquivo << "Sexo: " << strAnimal[i].sexo << endl;
+        arquivo << "Idade: " << strAnimal[i].idade << endl;
+        arquivo << "Nome do DONO: " << strAnimal[i].nomedono << endl;
+        arquivo << "RG do DONO: " << strAnimal[i].rg << endl;
+        arquivo << "Telefone: " << strAnimal[i].telefone << endl << endl;
+    }
+
+    arquivo.close();
+
+    cout << "Arquivo criado com Sucesso!" << endl;
+
+    return 0;
+}
